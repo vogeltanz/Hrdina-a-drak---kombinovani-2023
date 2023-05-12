c96d570637b31a8755acf7f0ea1f934eeb008b83
FunTom <vogeltanz@utb.cz>
2023-05-12 10:22:42 +0200

    lambda výrazy

--- Hrdina a drak - kombinovani/Program.cs
+++ Hrdina a drak - kombinovani/Program.cs
@@ -6,9 +6,30 @@ using Hrdina_a_drak___kombinovani.Veci;
 Mec mec = new Mec(25);
 
 Hrdina hrdina = new Hrdina("Geralt", 100, 5, 2, mec, Kostka.Instance);
-Hrdina hrdina2 = new Hrdina("Superhero", 100, 5, 5, mec, Kostka.Instance);
+Hrdina hrdina2 = new Hrdina("Superhero", 100, 1, 5, mec, Kostka.Instance);
 Drak drak = new Drak("Alduin", 150, 10, 2, Kostka.Instance);
-Vlk vlk = new Vlk("Wolfie", 50, 5, 1, Kostka.Instance);
+Vlk vlk = new Vlk("Wolfie", 50, 1, 1, Kostka.Instance);
+
+
+List<Postava> postavy = new List<Postava>()
+{
+    hrdina,
+    hrdina2,
+    drak,
+    vlk
+};
+
+double prumernyUtokPostav =
+    postavy.Average(postava => postava.MaxUtok);
+double polovinaPrumUtoku = prumernyUtokPostav / 2;
+
+Console.WriteLine($"Průměrný maximální útok postav: {prumernyUtokPostav}");
+
+List<Postava> vyfiltrovanySeznamPostav =
+    postavy.FindAll(postava => postava.MaxUtok >= polovinaPrumUtoku);
+
+vyfiltrovanySeznamPostav.ForEach(postava => Console.WriteLine($"{postava.Jmeno} má maximální útok: {postava.MaxUtok}"));
+
 
 Arena arena = new Arena(hrdina, vlk);
 arena.Boj();
